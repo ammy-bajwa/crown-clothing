118555763e4761a40d30a2ddd339a26acb10cc67
Amir Sadiq Ali <33599273+ammy-bajwa@users.noreply.github.com>
2019-11-06 10:46:05 +0500

    Adding homepage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Homepage from "./homepage.component";
 
 function App() {
   return (
     <div>
-      Hello Wordls
+      <Homepage />
     </div>
   );
 }
