fba59db53aa960bc620d5fc4fbdd0697af975099
Amir Sadiq Ali <33599273+ammy-bajwa@users.noreply.github.com>
2019-12-06 23:40:47 +0500

    Adding remove item from checkout page functionality

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -13,11 +13,19 @@ const cartReducer = (state = INITIAL_STATE, action) => {
         ...state,
         hidden: !state.hidden
       };
+
     case CartActionTypes.ADD_ITEM:
       return {
         ...state,
         cartItems: addItemToCart(state.cartItems, action.payload)
       };
+
+    case CartActionTypes.CLEAR_ITEM_FROM_CART:
+      return {
+        ...state,
+        cartItems: state.cartItems.filter(item => item.id !== action.payload.id)
+      };
+
     default:
       return state;
   }
