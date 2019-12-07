d587eb507be5c8adf352b0c7dbf0d062f969cec6
Amir Sadiq Ali <33599273+ammy-bajwa@users.noreply.github.com>
2019-12-07 23:57:22 +0500

    Adding payment with stripe and updating icon in index.html

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+import "./stripe-button.styles.scss";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = "pk_test_UJIW4jIiY9vAZXWxC2GsqHDT0099SPM9FR";
+
+  const onToken = token => {
+    console.log(token);
+    alert("Payment Successful");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="Crown Clothing Ltd."
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
